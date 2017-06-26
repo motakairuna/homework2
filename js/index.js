e7641ae8695bbd55b2abdc59e2a05eaae17c8f6f
Iryna <motakairuna@gmail.com>
2017-06-26 13:45:08 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -8,10 +8,8 @@ var c = Number(prompt('Введіть коефіцієнт с'));
 var resultNunber=discriminant(a,b,c);
 document.write(resultNunber);
 
-   function discriminant(a,b,c) {
-   var result;
-   var x1;
-   var x2;
+  function discriminant(a,b,c) {
+   var result, x1, x2;
    
    var d = b*b-4*a*c;
 
@@ -19,22 +17,20 @@ document.write(resultNunber);
      result='рівняння не квадратне';
      return result;
      }
-    else
-    if(d>0)
-    {
-     x1=(-b+Math.sqrt(d))/(2*a);
-     x2=(-b-Math.sqrt(d))/(2*a);
-     result ='Рівняння має 2 корені: x1=' +x1+',' +'x2=' +x2;
-     return result;
-}else{
- result ='Рівняння  не має дійсних коренів';
-     return result;
-}
-}
+     else
+     if(d>0)
+      {
+      x1=(-b+Math.sqrt(d))/(2*a);
+      x2=(-b-Math.sqrt(d))/(2*a);
+      
+      x1=Math.round(x1);
+      x2=Math.round(x2);
+      result ='Рівняння має 2 корені: x1=' + x1+',' +'x2=' +x2;
+      return result;
+      }else{
+       result ='Рівняння  не має дійсних коренів';
+       return result;
+       }
+     }
 
-//else{ 
-//    var n =b-c ;
-//  return n;
-//}
-//}
 
