0adc7c51ca69b7907d05669bd1d24f280a7f283f
Iryna <motakairuna@gmail.com>
2017-06-26 04:18:01 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// JavaScript File
+
+var a = Number(prompt('Введіть коефіцієнт а'));
+
+var b = Number(prompt('Введіть коефіцієнт в'));
+
+var c = Number(prompt('Введіть коефіцієнт с'));
+var resultNunber=discriminant(a,b,c);
+document.write(resultNunber);
+
+   function discriminant(a,b,c) {
+   var result;
+   var x1;
+   var x2;
+   
+   var d = b*b-4*a*c;
+
+    if(a==0){
+     result='рівняння не квадратне';
+     return result;
+     }
+    else
+    {
+     x1=(-b+Math.sqrt(d))/(2*a);
+     x2=(-b-Math.sqrt(d))/(2*a);
+     result ='Рівняння має 2 корені: x1=' +x1+',' +'x2=' +x2;
+     return result;
+}
+}
+//else{ 
+//    var n =b-c ;
+//  return n;
+//}
+//}
+
